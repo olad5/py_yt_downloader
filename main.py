c506969e61cadcd724f8be2673e96e0979a25502
shonubioladipo@gmail.com <shonubioladipo@gmail.com>
2021-09-08 00:47:32 +0100

    Downloads video but no audio

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import youtube_dl
+import shutil
 from hurry.filesize import size
 import json
 import sys
+import pprint
 import logging
 
 logging.basicConfig(
@@ -10,21 +12,11 @@ logging.basicConfig(
 )
 
 
-# Quick demo on the youtube_dl module
-# Note this module is a downloader but this demo is using it as API to get data from 
-# Youtube
 
 
-class Youtube_data:
+class Youtube_download:
     def __init__(self):
-        # initialize youtube_dl
-        # self.ydl = youtube_dl.YoutubeDL()
-        # print(sys.argv[1])
-        # sys.exit()
-        # self.query_video()
         self.show_formats()
-        # self.print_data()
-        # self.save_to_json()
 
 
 
@@ -33,9 +25,7 @@ class Youtube_data:
         # Youtube link
         #  demo link 'https://www.youtube.com/watch?v=YSy2lBZ1QrA'
         self.url  = sys.argv[1]
-        self.ydl = youtube_dl.YoutubeDL(ydl_opts)
-        logging.debug(self.url)
-        logging.debug(type(self.url))
+        self.ydl = youtube_dl.YoutubeDL()
         # uses the youtube_dl as a context manager
         with self.ydl:
             self.result  =  self.ydl.extract_info(self.url,
@@ -50,45 +40,43 @@ class Youtube_data:
                 format_note = format['format_note']
                 full_info  = '    '.join([str( 'id=' + format_id ), str(format_note),
                                           str( ext), str(filesize)])
-                logging.debug(full_info)
+                print(full_info)
+            print()
+            self.request_id()
 
 
 
 
-    def save_to_json(self):
-        # this function saves the output to a json file in the current directory
-        # for easy viewing
-        # NOTE: it overwrites the file result.json everytime it is run
-        with open('data/result.json', 'w') as f:
-            json_data  = json.dumps(self.result, indent=4)
-            f.write(json_data)
+    def request_id(self):
+        select_id =  input("Pick an a video id to download \n")
+        # select_id = str ( 160 )
+        select_dict = [format for format in self.result['formats'] if format['format_id'] == select_id][0]
+        filesize=size( ( select_dict['filesize']  ))
+        url = select_dict['url']
+        print(f"Downloading {self.result['title']}, size={filesize}")
+        self.title = self.result['title']
+        for item in ["(", ")",  " ", ",", ".", "'"]:
+            logging.debug(item)
+            self.title  = self.title.replace(item, '_')
+        self.title  = self.title.replace('__', '_')
+        self.download_video(select_id)
 
-    def print_data(self):
-        # this function prints some important data we need about the Youtube
-        # video
-        # Title of the video
-        video_title  =self.result['title']
-        logging.debug(f"Video title is {video_title}\n")
+    def download_video(self,select_id):
+        ydl_opts ={
+            'format': select_id,
+            'outtmpl':self.title +  '.%(ext)s',
+        }
 
-        # video id for easy storage in database
-        video_id  =self.result['id']
-        logging.debug(f"Video id is {video_id}\n")
+        logging.debug(self.url)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([ self.url ])
+            shutil.move(self.title+'.mp4' ,'data')
 
-        # thumbnails that we can display for audio files
-        thumbnails  =self.result['thumbnails']
-        logging.debug(f"Video thumbnail links are  {thumbnails}\n")
 
-        # duration of the video
-        video_duration  =self.result['duration']
-        logging.debug(f"Video duration is {video_duration}\n")
-
-        # the artist name
-        artist  =self.result['artist']
-        logging.debug(f"Video artist is {artist}\n")
 
 
 
 if __name__ == '__main__':
     # initializes the class
-    yt = Youtube_data()
+    yt = Youtube_download()
 
